c884e9313214c1a0fcff65aec4342c013142cd10
TWINKLE PIE <22twinklepie@gmail.com>
2024-08-30 02:18:53 +0530

    Restored and updated Frontend submodule

--- target/classes/static/_next/static/chunks/app/page-b7fa09154fac3cdd.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,702,951],{9191:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,582,744],function(){return n(n.s=9191)}),_N_E=n.O()}]);
